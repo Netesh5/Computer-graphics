e3a40a46033d477cfe97efd0f4f25b30c7cee51e
netesh paudel <netesh.paudel@gmail.com>
2021-07-02 09:36:39 +0545

    circle

--- circle.cpp
+++ circle.cpp
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<graphics.h>
 int main(){
-	int x,y,r,p0;
+	int x1,y1,r,p0,x,y;
 	int gd=DETECT,gm;
 	initwindow(getmaxwidth()/2,getmaxheight());
     printf("Enter the center point : ");
-	scanf("%d%d",&x,&y);
+	scanf("%d%d",&x1,&y1);
 	printf("Enter the radius of circle :");
 	scanf("%d",&r);
     p0=1-r;
@@ -20,9 +20,19 @@ int main(){
             y--;
             p0+=2*x-2*y+1;
         }
-        putpixel(x,y,YELLOW);
-        delay(10);
+        putpixel(x+x1,y+y1,YELLOW);
+        putpixel(y+y1,x+x1,YELLOW);
+        putpixel(-y+y1,x+x1,YELLOW);
+        putpixel(-x+x1,y+y1,YELLOW);
+        putpixel(-x+x1,-y+y1,YELLOW);
+        putpixel(x+x1,-y+y1,YELLOW);
+        putpixel(-y+y1,x+x1,YELLOW);
+        putpixel(-y+y1,-x+x1,YELLOW);
+        putpixel(y+y1,-x+x1,YELLOW);
 
+
+
+        delay(10);
     }
 getch();
 
